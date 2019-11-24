99ea75e2716c70c672c8f001465f8b7e25d35f7f
Jim Strother <jim.strother@gmail.com>
2019-11-24 13:55:56 -0800

    first commit

---
+++ pages/admin/new-post/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="new-post-form">
+    <AdminPostForm />
+  </div>
+</template>
+
+<script>
+import AdminPostForm from '@/components/Admin/AdminPostForm';
+
+export default {
+  layout: 'admin',
+  components: {
+    AdminPostForm
+  }
+}
+</script>
+
+<style scoped>
+.new-post-form {
+  width: 90%;
+  margin: 20px auto;
+}
+
+@media (min-width: 768px) {
+  .new-post-form {
+    width: 500px;
+  }
+}
+</style>
---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="single-post">
+    <section class="post">
+      <h1 class="post-title">Title of Post</h1>
+      <div class="post-details">
+        <div class="post-detail">Last updated on XXX</div>
+        <div class="post-detail">Written by NAME</div>
+      </div>
+      <p class="post-content">Content of post</p>
+      <section class="post-feedback">
+        <p>Let me know what you think about the post. Send feedback to <a href="mailto:feedback@my-blog-site.com">feedback@my-blog-site.com</a></p>
+      </section>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+.single-post-page {
+  padding: 30px;
+  text-align: center;
+  box-sizing: border-box;
+}
+
+.post {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+
+@media (min-width: 768px) {
+  .post {
+    width: 600px;
+    margin: auto;
+  }
+}
+
+.post-title {
+  margin: 0;
+}
+
+.post-details {
+  padding: 10px;
+  box-sizing: border-box;
+  border-bottom: 3px solid #ccc;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+@media (min-width: 768px) {
+  .post-details {
+    flex-direction: row;
+  }
+}
+
+.post-detail {
+  color: rgb(88, 88, 88);
+  margin: 0 10px;
+}
+
+.post-content {
+  align-self: center;
+  padding: 0.5em;
+}
+
+.post-feedback a {
+  color: red;
+  text-decoration: none;
+}
+
+.post-feedback a:hover,
+.post-feedback a:active {
+  color: salmon;
+}
+</style>
